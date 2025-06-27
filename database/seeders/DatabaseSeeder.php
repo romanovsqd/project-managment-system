f0278b15cc7bbc34d52d9ab26bae238ffc90b2a9
Oleg Romanov <sqdcontrast@gmail.com>
2025-06-27 19:51:57 +0300

    feat(seeders): implement DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = User::factory(2)->create(['role' => 'manager']);
+
+        $user->each(function ($user) {
+            $project = Project::factory()->create([
+                'created_by' => $user->id,
+            ]);
+
+            $user->memberProjects()->attach($project);
+
+            $projectMembers = User::factory(5)->create();
+
+            $projectMembers->each(function ($projectMember) use ($project) {
+                $projectMember->memberProjects()->attach($project);
+                Task::factory(2)->create([
+                    'project_id' => $project->id,
+                    'assigned_to' => $projectMember->id,
+                ]);
+            });
+        });
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@mail.com',
+            'role' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'email' => 'manager@mail.com',
+            'role' => 'manager',
+        ]);
+
+        User::factory()->create([
+            'email' => 'member@mail.com',
+            'role' => 'member',
         ]);
     }
 }
