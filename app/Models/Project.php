fc191141cd5561d44b89ccd9ca0250d44032d1b2
Oleg Romanov <sqdcontrast@gmail.com>
2025-06-27 17:41:11 +0300

    feat(projects): add Project model

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'created_by',
+    ];
+
+    protected $attributes = [
+        'status' => 'active',
+    ];
+}
