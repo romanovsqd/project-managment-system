ec432f314ee92af95aa080bc5dbead5a0a87e145
Oleg Romanov <sqdcontrast@gmail.com>
2025-06-27 17:49:15 +0300

    feat(tasks): add TaskFactory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'project_id' => Project::factory()->create(),
+            'title' => fake()->words(3, true),
+            'description' => fake()->paragraph(),
+            'status' => 'to_do',
+            'assigned_to' => User::factory()->create(),
+        ];
+    }
+}
