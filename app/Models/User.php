1558f33c3b21d0b39ac9d830a73bee0aed57bc36
Oleg Romanov <sqdcontrast@gmail.com>
2025-06-30 17:54:02 +0300

    feat(users): add role to fillable and attributes properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
+    ];
+
+    protected $attributes = [
+        'role' => 'member',
     ];
 
     /**
